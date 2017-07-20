ea904ee1851280afd8202c05692a0737a8b89daf
棒棒勝 <jayuiop@gmail.com>
2017-07-20 20:55:58 +0800

    整理了一點秋番，改了新格式

---
+++ anime2017.10.js
@@ -0,0 +1,77 @@
+//==============================//
+//          2017 夏番清單
+//  github.com/gnehs/Anime-List
+//==============================//
+//            資料來源
+// 【秋番】2017年10月新番一覽（日本秋季新番列表）
+// http://justlaughtw.blogspot.com/2017/03/201710.html
+// 維基百科
+// https://zh.wikipedia.org/
+//==============================//
+Anime = [
+    // 星期格式：
+    //     Sun  Mon Tue  Wed  Thu  Fri  Sat none
+    //     日   一   二   三    四   五   六  尚未公開
+    // 原作載體：
+    //     Comic  Novel   Game  Original
+    //    漫畫    小說    遊戲   原創或其他
+    // 資料存取範例：
+    //    -> Anime[0]['name']
+    //    <- LoveLive! SunShine!!
+    {
+        name: 'LoveLive! SunShine!!',
+        date: '尚未公開',
+        week: 'none',
+        time: '尚未公開',
+        carrier: 'Original',
+        season: '2',
+        nameInJpn: 'LoveLive! SunShine!!',
+        img: 'http://i.imgur.com/FbMfs4G.png',
+        description: "故事以日本靜岡縣的浦之星女學院為故事舞台，描述憧憬著 μ's 的主角之一高海千歌，將在這小地方與夥伴們朝著校園偶像的目標邁進。",
+    },
+    {
+        name: '血界戰線',
+        date: '尚未公開',
+        week: 'none',
+        time: '尚未公開',
+        carrier: 'Comic',
+        season: '2',
+        nameInJpn: '血界戦線',
+        img: 'https://i.imgur.com/qo03WHb.png',
+        description: '紐約一晚便消失無蹤，取而代之的是異界與人界連接點「赫爾沙雷姆茲·羅特」，秘密結社萊布拉為了世界平衡和守護正常秩序而出動。',
+    },
+    {
+        name: '鬼燈的冷徹',
+        date: '尚未公開',
+        week: 'none',
+        time: '尚未公開',
+        carrier: 'Comic',
+        season: '2',
+        nameInJpn: '鬼灯の冷徹',
+        img: 'https://i.imgur.com/5mjlBsI.png',
+        description: '故事描述戰後人口爆發且惡靈兇暴化，亡者產生的問題使的地獄出現了前所未有混亂；身為閻羅王手下的第一補佐官鬼燈，將替代閻羅王將解決各式麻煩的問題，地獄也每天都上演著閻羅王翹班挨罵的情景。',
+    },
+    {
+        name: '3 月的獅子',
+        date: '尚未公開',
+        week: 'none',
+        time: '尚未公開',
+        carrier: 'Comic',
+        season: '2',
+        nameInJpn: '3月のライオン',
+        img: 'https://i.imgur.com/QID5pXV.png',
+        description: '描述獨自居住在東京舊市鎮的 17 歲少年 桐山零，被周遭歌頌為天才將棋棋士的零，在幼時因意外事故失去了家人，所以生活上、還是棋盤上都背負著深沉的孤獨。在這樣的他面前卻出現了明里、日向、桃三姊妹......',
+    },
+    /*{
+        name: '123',
+        date: '尚未公開',
+        week: 'none',
+        time: '尚未公開',
+        carrier: '123',
+        season: '1232',
+        nameInJpn: '123',
+        img: '123',
+        description: '123',
+    },*/
+
+]
