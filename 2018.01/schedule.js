056d9335c374f62153c54b8c8d03d081928431a3
GNEHS <jayuiop@gmail.com>
2017-12-23 15:07:51 +0800

    現在日程表會按照日期排列了

--- 2018.01/schedule.js
+++ 2018.01/schedule.js
@@ -1,14 +1,18 @@
 //=========================//
 //           輸出
 //=========================//
+
+var AnimeSorted = Anime.sort(function(a, b) {
+    return a.date.split("/")[1] - b.date.split("/")[1];
+});
 $(function() {
     // 透過迴圈輸出資料內的所有動畫
     // Anime.length = 動畫總數
     // 迴圈開始
     for (i = 0; i < Anime.length; i = i + 1) {
-        var Anime_Name = Anime[i].name; //動畫名稱
-        var Anime_Day = Anime[i].week; //星期
-        var Anime_Date = Anime[i].date; //星期
+        var Anime_Name = AnimeSorted[i].name; //動畫名稱
+        var Anime_Day = AnimeSorted[i].week; //星期
+        var Anime_Date = AnimeSorted[i].date; //星期
         if (Anime[i].week != 'none') { //看看有沒有公布星期
             $("#" + Anime_Day)
                 .append($("<div/>")
