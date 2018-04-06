7325d7b131408729739475e9b29f5ba519a67b1c
GNEHS <jayuiop@gmail.com>
2018-04-06 14:40:48 +0800

    ..

--- sw.js
+++ sw.js
@@ -1,23 +1,48 @@
 var SLOW_TIME = 3000;
 
-this.addEventListener('install', function() {
+this.addEventListener('install', function(event) {
     console.log('Installed service worker');
+    event.waitUntil(
+        caches.open('al-cache').then(function(cache) {
+            return cache.addAll([
+                '/',
+                './index.html',
+                './css/style.css',
+                './js/index.js',
+                './2018.01/anime2018.01.min.js', './2018.04/anime2018.04.min.js', './2017.10/anime2017.10.min.js', './2017.07/anime2017.07.min.js'
+            ]);
+        })
+    );
 });
-
 this.addEventListener('fetch', function(event) {
-    var url = event.request.url;
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            // 来来来，代理可以搞一些代理的事情
 
-    if (url.indexOf('blocking') === -1) {
-        return;
-    }
+            // 如果 Service Worker 有自己的返回，就直接返回，减少一次 http 请求
+            if (response) {
+                return response;
+            }
 
-    var promise = Promise.race([
-        new Promise((resolve, reject) => setTimeout(
-            () => reject(new Response('Request killed!')),
-            SLOW_TIME
-        )),
-        fetch(event.request),
-    ]);
+            // 如果 service worker 没有返回，那就得直接请求真实远程服务
+            var request = event.request.clone(); // 把原始请求拷过来
+            return fetch(request).then(function(httpRes) {
 
-    event.respondWith(promise);
+                // http请求的返回已被抓到，可以处置了。
+
+                // 请求失败了，直接返回失败的结果就好了。。
+                if (!httpRes || httpRes.status !== 200) {
+                    return httpRes;
+                }
+
+                // 请求成功的话，将请求缓存起来。
+                var responseClone = httpRes.clone();
+                caches.open('my-test-cache-v1').then(function(cache) {
+                    cache.put(event.request, responseClone);
+                });
+
+                return httpRes;
+            });
+        })
+    );
 });
