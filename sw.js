072c580601fd4b3a08b1792827f4ff1ab09dd564
GNEHS <jayuiop@gmail.com>
2018-04-06 14:42:52 +0800

    kkk.

--- sw.js
+++ sw.js
@@ -17,27 +17,27 @@ this.addEventListener('install', function(event) {
 this.addEventListener('fetch', function(event) {
     event.respondWith(
         caches.match(event.request).then(function(response) {
-            // 来来来，代理可以搞一些代理的事情
+            // 來來來，代理可以搞一些代理的事情
 
-            // 如果 Service Worker 有自己的返回，就直接返回，减少一次 http 请求
+            // 如果 Service Worker 有自己的返回，就直接返回，減少一次 http 請求
             if (response) {
                 return response;
             }
 
-            // 如果 service worker 没有返回，那就得直接请求真实远程服务
-            var request = event.request.clone(); // 把原始请求拷过来
+            // 如果 service worker 沒有返回，那就得直接請求真實遠程服務
+            var request = event.request.clone(); // 把原始請求拷過來
             return fetch(request).then(function(httpRes) {
 
-                // http请求的返回已被抓到，可以处置了。
+                // http請求的返回已被抓到，可以處置了。
 
-                // 请求失败了，直接返回失败的结果就好了。。
+                // 請求失敗了，直接返回失敗的結果就好了。。
                 if (!httpRes || httpRes.status !== 200) {
                     return httpRes;
                 }
 
-                // 请求成功的话，将请求缓存起来。
+                // 請求成功的話，將請求緩存起來。
                 var responseClone = httpRes.clone();
-                caches.open('my-test-cache-v1').then(function(cache) {
+                caches.open('al-cache').then(function(cache) {
                     cache.put(event.request, responseClone);
                 });
 
