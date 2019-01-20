1afd1a4e2f2f734ae389ba962f5d6c387db7d25a
t510599 <t510599@gmail.com>
2019-01-21 00:47:00 +0800

    Detect season when fetching data

--- animen.js
+++ animen.js
@@ -1,20 +1,25 @@
-json = `[`
-for (i = 0; i < $("#item-list .one-shiban").length; i++) {
-    let $anime = $("#item-list .one-shiban").eq(i)
-    let img = $anime.children("a").children("img").attr("src")
-    let name = $anime.children("a").children(".book-name").text()
-    let jpName = $anime.children(".author").children("span").text()
-    json += `{"name": "${name}",
-    "nameInJpn": "${jpName}",
-    "date": "4/",
-    "time": "none",
-    "carrier": "Comic Novel Game Original",
-    "season": 1,
-    "episode": 0,
-    "img": "${img}",
-    "official": "",
-    "description": ""
-},`
-}
-json = json.replace(/\n|\r/, '') + `]`
-console.log(json)
+var result = [];
+$("#item-list .one-shiban").each((_i, e) => {
+    let $anime = $(e);
+    let imgSrc = $anime.children("a").children("img").attr("src");
+    let name = $anime.children("a").children(".book-name").text();
+    let jpName = $anime.children(".author").children("span").text();
+
+    let regex = /\s第([1-9]+)期$/;
+    let season = (regex.test(name)) ? parseInt(regex.exec(name)[1]) : 1;
+    animeData = {
+        "name": name,
+        "nameInJpn": jpName,
+        "date": "4/",
+        "time": "none",
+        "carrier": "Comic Novel Game Original",
+        "season": season,
+        "episode": 0,
+        "img": imgSrc,
+        "official": "",
+        "description": ""
+    }
+    result.push(animeData);
+});
+
+console.log(JSON.stringify(result, null, 2));
